15c511f9bc3b7882d4179338779d3afd6150f36f
tin33392js <calsharp0202@yahoo.com>
2022-11-21 11:14:13 +0700

    tell the source of filepath

--- my-first-io.js
+++ my-first-io.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+// process.argv[2] as path is from homework
 const filepath = process.argv[2];
 const regex = /\n/g;
 const data = fs.readFileSync(filepath, { encoding: 'utf-8' });
